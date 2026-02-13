0975ab5e1b3f695643fc331a757765f642f11705
nandankrishna11 <nandankrishna7034@gmail.com>
2026-02-13 11:19:58 +0530

    13/2/26

--- src/controlStatement/NestedIf.java
+++ src/controlStatement/NestedIf.java
@@ -2,7 +2,9 @@ package controlStatement;
 
 public class NestedIf {
     public static void main(String[] args) {
-        double accBal=10000.0;
+
+        //withdrawal problem
+       /* double accBal=10000.0;
         int amt=130;
         if(amt<=accBal )
         {
@@ -19,6 +21,30 @@ public class NestedIf {
             System.out.println("insuffecient bal");
         }
 
+        */
+//age eligibility
+
+        int age=25;
+        char ch='M';
+        if(ch=='M'){
+            if(age>21){
+                System.out.println("eligible to marriage");
+            }
+            else {
+                System.out.println("not eligible for marriage");
+            }
+        }
+        else if(ch=='F'){
+            if(age>=18){
+                System.out.println("eligible spinster");
+            }
+            else{
+                System.out.println("girl underage");
+            }
+        }
+        else{
+            System.out.println("invalid gender");
+        }
     }
 
 }
