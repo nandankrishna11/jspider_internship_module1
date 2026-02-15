c66b5453f5ec016e055ca339f5eff2f154400bc3
nandankrishna11 <nandankrishna7034@gmail.com>
2026-02-15 11:26:20 +0530

    15/2/26

--- src/controlStatement/PatternProblem/SquarePattern.java
+++ src/controlStatement/PatternProblem/SquarePattern.java
@@ -158,6 +158,55 @@ Q5) to print -
             System.out.println();
         }
 
-*/
+Q8)
+1 1 1 1 1
+0 0 0 0 0
+1 1 1 1 1
+0 0 0 0 0
+1 1 1 1 1
+
+
+
+int n=5;
+for (int i=0;i<n;i++)
+{
+    for(int j=0;j<n;j++){
+        if (i%2==0){
+            System.out.print("1"+" ");
+        }
+        else {
+            System.out.print("0"+" ");
+        }
+
+    }
+    System.out.println();
+}
+
+Q9)
+
+1 0 1 0 1
+1 0 1 0 1
+1 0 1 0 1
+1 0 1 0 1
+1 0 1 0 1
+
+
+        int n=5;
+        for (int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++){
+                if (j%2==0){
+                    System.out.print("1"+" ");
+                }
+                else {
+                    System.out.print("0"+" ");
+                }
+
+            }
+            System.out.println();
+        }
+
+
+ */
     }
 }
