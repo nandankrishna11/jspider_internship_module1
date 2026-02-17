3404cbe83c7c8bc8cd56bf910205bcdca90be550
nandankrishna11 <nandankrishna7034@gmail.com>
2026-02-17 18:49:18 +0530

    17/2/26

---
+++ src/controlStatement/WhileLoop.java
@@ -0,0 +1,49 @@
+package controlStatement;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        /*int a=1;
+        while (a<=5){
+            System.out.println(a);
+            a++;
+
+        }
+//odd number program
+
+        int y=10;
+        int b=1;
+        while(y>=b){
+            if(y%2!=0){
+                System.out.println(y);
+            }
+            y--;
+        }
+
+//program to find factorial
+
+        int n=5;
+        int fact=1;
+        while(n>=1){
+            fact=fact*n;
+            n--;
+        }
+        System.out.println(fact);
+
+
+
+
+         */
+
+//sum program
+
+        int a=1;
+        int b=5;
+        int sum=0;
+        while(a<=b){
+            sum=sum +a;
+            a++;
+        }
+        System.out.println(sum);
+    }
+
+}
