c1b0d1abae1b8e69c10c775925624823dd3afdac
nandankrishna11 <nandankrishna7034@gmail.com>
2026-02-27 10:48:56 +0530

    27-2-26 "String"

---
+++ src/MODULE1/STRINGS/P5_toUpperCase.java
@@ -0,0 +1,12 @@
+package MODULE1.STRINGS;
+
+public class P5_toUpperCase {
+    public static void main(String[] args) {
+        String str="Java_SE25";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str);
+        String str1=str.toUpperCase();
+        System.out.println(str1);
+    }
+}
