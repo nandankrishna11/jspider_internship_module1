dca8e0effe3609e2cbca5dfcd1c11407fe03b3a2
nandankrishna11 <nandankrishna7034@gmail.com>
2026-03-01 15:35:28 +0530

    Added Demo class for reference

---
+++ src/MODULE2/CLASSandOBJECT/Demo.java
@@ -0,0 +1,18 @@
+package MODULE2.CLASSandOBJECT;
+
+class Demo {
+    static int a = 10; // Static variable
+    int b = 10;        // Non-static (instance) variable
+
+    // Static method
+    public static void test() {
+        // code here
+        int x = 10;    // Local variable
+    }
+
+    // Non-static method
+    public void display() {
+        // code here
+        int y = 20;    // Local variable
+    }
+}
