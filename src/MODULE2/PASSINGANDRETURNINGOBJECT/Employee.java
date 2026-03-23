48be03dbc089ea318fecbd1e855d07c3d37217a3
nandankrishna11 <nandankrishna7034@gmail.com>
2026-03-23 15:42:58 +0530

    Ex1-Passing and return object

---
+++ src/MODULE2/PASSINGANDRETURNINGOBJECT/Employee.java
@@ -0,0 +1,10 @@
+package MODULE2.PASSINGANDRETURNINGOBJECT;
+
+public class Employee {
+    int id;
+    double ctc;
+    Employee(int id,double ctc){
+        this.id=id;
+        this.ctc=ctc;
+    }
+}
---
+++ src/MODULE2/PASSINGANDRETURNINGOBJECT/Hr.java
@@ -0,0 +1,20 @@
+package MODULE2.PASSINGANDRETURNINGOBJECT;
+
+import java.util.Scanner;
+
+public class Hr {
+    static void displayEmployeeDetails(Employee e){
+        if(e!=null){
+            System.out.println("EID="+e.id);
+            System.out.println("CTC="+e.ctc);
+        }
+    }
+    static Employee createEmployee(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter id");
+        int id=sc.nextInt();
+        System.out.println("Enter CTC");
+        Double ctc=sc.nextDouble();
+        return new Employee(id,ctc);
+    }
+}
