2ab1c5efd47494efdd72a6abc611fc79d623f3ac
nandankrishna11 <nandankrishna7034@gmail.com>
2026-04-01 15:36:06 +0530

    Overriding

---
+++ src/MODULE2/overriding/marriage/Mainclass3.java
@@ -0,0 +1,22 @@
+package MODULE2.overriding.marriage;
+class  Mother{
+    void marriage(){
+        System.out.println("arranged marriage");
+    }
+}
+class Daughter extends Mother{
+    void marriage(){
+        System.out.println("love marriage");
+    }
+}
+
+public class Mainclass3 {
+    public static void main(String[] args) {
+        Mother m= new Mother();
+        m.marriage();
+        Daughter d=new Daughter();
+        d.marriage();
+        Mother ref=new Daughter();
+        ref.marriage();
+    }
+}
