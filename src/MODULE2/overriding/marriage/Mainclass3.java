1e38aaad8757b42aedc119b2e9abe176da1c1e20
nandankrishna11 <nandankrishna7034@gmail.com>
2026-04-01 15:37:59 +0530

    Overriding

--- src/MODULE2/overriding/marriage/Mainclass3.java
+++ src/MODULE2/overriding/marriage/Mainclass3.java
@@ -8,6 +8,9 @@ class Daughter extends Mother{
     void marriage(){
         System.out.println("love marriage");
     }
+    void drinking(){
+        System.out.println("coffee/tea");
+    }
 }
 
 public class Mainclass3 {
@@ -16,7 +19,9 @@ public class Mainclass3 {
         m.marriage();
         Daughter d=new Daughter();
         d.marriage();
+        d.drinking();
         Mother ref=new Daughter();
         ref.marriage();
+        //ref.drinking(); -> error mother cant access daughter property
     }
 }
