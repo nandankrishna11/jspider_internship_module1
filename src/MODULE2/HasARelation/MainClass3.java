e0f60575712d1953ccf22c059e4bae990b0fa20d
nandankrishna11 <nandankrishna7034@gmail.com>
2026-04-04 15:29:48 +0530

    Add MainClass3 to demonstrate Has-A relationship with Alpha and Beta classes

---
+++ src/MODULE2/HasARelation/MainClass3.java
@@ -0,0 +1,27 @@
+package MODULE2.HasARelation;
+class Alpha{
+    void help(){
+            System.out.println("Executing help method of Alpha class");
+    }
+}
+
+class Beta{
+    void assist(){
+        System.out.println("Executing assist method of Beta class");
+    }
+}
+class Gamma{
+    Alpha a=new Alpha();
+    Beta b;
+    Gamma(Beta b){
+        this.b=b;
+    }
+}
+public class MainClass3 {
+    public static void main(String[] args) {
+        Beta b1=new Beta();
+        Gamma g1=new Gamma(b1);
+        g1.a.help();
+        g1.b.assist();
+    }
+}
